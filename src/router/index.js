d39335455bb191ffa14761e01bbd33391ae88286
Andre Asselin <andre.asselin@pointsource.com>
2017-02-17 21:41:11 -0500

    Add code splitting and change assetsPublicPath to ./

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from 'components/Hello'
 
 Vue.use(Router)
+
+const Hello = resolve => {
+  // require.ensure is Webpack's special syntax for a code-split point.
+  require.ensure(['components/Hello'], () => {
+    resolve(require('components/Hello'))
+  })
+}
 
 export default new Router({
   routes: [
